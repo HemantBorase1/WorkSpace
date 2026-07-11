7b8637eac1e957db0bb9cf07f06797e6454fd9b9
Hemant Borase <hborase004@gmail.com>
2026-07-11 16:08:00 +0530

    LinkedList Code

--- LinkListDemo/src/main/java/DSA/LinkListDemo/Node.java
+++ LinkListDemo/src/main/java/DSA/LinkListDemo/Node.java
@@ -2,11 +2,12 @@ package DSA.LinkListDemo;
 
 public class Node<E> {
 
-    public E data;
-    public Node<E> next;
-
-    public Node(E e) {
-        data = e;
-        next = null;
-    }
+	public E data;
+	public Node<E> next;
+	
+	public Node(E e) {
+		data=e;
+		next=null;
+	}
+    
 }
